00cda2457c768c65aee9feb4b0d5d82dbab22e25
TommyKaefi <108281474+TommyKaefi@users.noreply.github.com>
2023-10-26 14:13:38 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
 
 # Read the data
-df1 = pd.read_csv('/Q16.csv', delimiter=';')
-df2 = pd.read_csv('/Q19.csv', delimiter=';')
+df1 = pd.read_csv('/Users/tomkafer/Desktop/Q16.csv', delimiter=';')
+df2 = pd.read_csv('/Users/tomkafer/Desktop/Q19.csv', delimiter=';')
 
 # Filtering out error values
 valid_range = set(range(1, 7)) # 1-5 and 6 for "no opinion"
@@ -17,19 +17,6 @@ for col in columns_q1:
 
 for col in columns_q2:
     df2 = df2[df2[col].isin(valid_range)]
-
-correlations = {}
-
-for col1 in columns_q1:
-    for col2 in columns_q2:
-        correlation = df1[col1].corr(df2[col2], method='spearman')
-        correlations[(col1, col2)] = correlation
-
-# Convert the correlations dictionary to a matrix form (DataFrame)
-correlation_matrix = pd.DataFrame(index=columns_q1, columns=columns_q2)
-for key, value in correlations.items():
-    correlation_matrix.at[key[0], key[1]] = value
-correlation_matrix = correlation_matrix.astype(float)
 
 # For Chi-square results
 chi2_matrix = pd.DataFrame(index=columns_q1, columns=columns_q2)
@@ -51,6 +38,5 @@ ax.set_title('Chi-square Test Results Matrix')
 ax.set_xlabel('Q19')
 ax.set_ylabel('Q16', rotation=0, labelpad=40)
 ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
-
 
 plt.show()
