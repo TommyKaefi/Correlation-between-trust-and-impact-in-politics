58fe7ef8f895d9a5d57016d30401a48547bcbded
TommyKaefi <108281474+TommyKaefi@users.noreply.github.com>
2023-10-26 16:57:40 +0200

    Update analysis.py
    Added LogNorm for better visualization

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import chi2_contingency
+from matplotlib.colors import LogNorm
 
 # Read the data
 df1 = pd.read_csv('/Users/tomkafer/Desktop/Q16.csv', delimiter=';')
@@ -12,26 +13,31 @@ valid_range = set(range(1, 7)) # 1-5 and 6 for "no opinion"
 columns_q1 = ['A', 'B', 'C']
 columns_q2 = ['A', 'B', 'C']
 
-for col in columns_q1:
-    df1 = df1[df1[col].isin(valid_range)]
 
+for col in columns_q1:
+        df1 = df1[df1[col].isin(valid_range)]
+    
 for col in columns_q2:
-    df2 = df2[df2[col].isin(valid_range)]
+        df2 = df2[df2[col].isin(valid_range)]
+
 
 # For Chi-square results
 chi2_matrix = pd.DataFrame(index=columns_q1, columns=columns_q2)
-p_value_matrix = pd.DataFrame(index=columns_q1, columns=columns_q2)
 for col1 in columns_q1:
     for col2 in columns_q2:
         contingency = pd.crosstab(df1[col1], df2[col2])
-        chi2, p, _, _ = chi2_contingency(contingency)
-        chi2_matrix.at[col1, col2] = chi2
+        chi2, p, _, _ = chi2_contingency(contingency, correction= True)
+        chi2_matrix.at[col1, col2] = p
 chi2_matrix = chi2_matrix.astype(float)
 
+#
+
+# Apply SymLogNorm normalization
+norm = LogNorm(vmin=chi2_matrix.min().min(), vmax=chi2_matrix.max().max())
 
 # Plot the chi2 matrix
 fig, ax = plt.subplots(figsize=(10, 8))
-sns.heatmap(chi2_matrix, annot=True, cmap = 'Grays', ax=ax) #cmap='Oranges'
+sns.heatmap(chi2_matrix, annot=True, cmap='gray', ax=ax, norm=norm)
 ax.set_title('Chi-square Test Results Matrix')
 ax.set_xlabel('Q19')
 ax.set_ylabel('Q16', rotation=0, labelpad=40)
