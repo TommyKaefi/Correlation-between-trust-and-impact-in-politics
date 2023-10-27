07adecc7b9e07bf2623ef8686250d3c232ca1828
TommyKaefi <108281474+TommyKaefi@users.noreply.github.com>
2023-10-27 18:15:19 +0200

    Update analysis.py
    summary and contingency tables

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import chi2_contingency
 from matplotlib.colors import LogNorm
 
@@ -13,7 +14,6 @@ valid_range = set(range(1, 7)) # 1-5 and 6 for "no opinion"
 columns_q1 = ['A', 'B', 'C']
 columns_q2 = ['A', 'B', 'C']
 
-
 for col in columns_q1:
         df1 = df1[df1[col].isin(valid_range)]
     
@@ -21,13 +21,53 @@ for col in columns_q2:
         df2 = df2[df2[col].isin(valid_range)]
 
 
+# Initialize an empty DataFrame to store the mean and median values
+summary_table = pd.DataFrame(index=['Mean', 'Median'])
+
+# Calculate mean and median values for each column and add them to the summary_table
+for col1, col2 in zip(columns_q1, columns_q2):
+    summary_table[f'Q16{col1}'] = [df1[col1].mean(), df1[col1].median()]
+    summary_table[f'Q19{col2}'] = [df2[col2].mean(), df2[col2].median()]
+
+# Format the cell text to have three decimal places
+formatted_values = summary_table.applymap('{:.3f}'.format).values
+
+# Plot the summary table as a plain table
+fig, ax = plt.subplots(figsize=(8, 6))  
+ax.axis('off') 
+tbl = ax.table(cellText=formatted_values,
+               colLabels=summary_table.columns,
+               rowLabels=summary_table.index,
+               cellLoc='center',  
+               loc='center')  
+tbl.auto_set_font_size(False)
+tbl.set_fontsize(16)  
+tbl.scale(1.2, 1.2)  
+plt.title('Summary Table of Mean and Median Values', fontsize=18, pad=20) 
+plt.show()
+
 # For Chi-square results
 chi2_matrix = pd.DataFrame(index=columns_q1, columns=columns_q2)
+
+
+# Chi-Square results and contingency table
 for col1 in columns_q1:
     for col2 in columns_q2:
         contingency = pd.crosstab(df1[col1], df2[col2])
-        chi2, p, _, _ = chi2_contingency(contingency, correction= True)
+
+        # Plot the contingency tables
+        fig, ax = plt.subplots(figsize=(10, 8))
+        sns.heatmap(contingency, annot=True, fmt='d', cmap='gray', ax=ax)
+        ax.set_title(f'Contingency Table: Q16 {col1} & Q19 {col2}')
+        ax.set_xlabel('Q19')
+        ax.set_ylabel('Q16', rotation=0, labelpad=40)
+        ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+        print(contingency)
+        plt.show()
+
+        chi2, p, _, _ = chi2_contingency(contingency, correction=True)
         chi2_matrix.at[col1, col2] = p
+
 chi2_matrix = chi2_matrix.astype(float)
 
 # Apply SymLogNorm normalization
@@ -40,6 +80,5 @@ ax.set_title('Chi-square Test Results Matrix')
 ax.set_xlabel('Q19')
 ax.set_ylabel('Q16', rotation=0, labelpad=40)
 ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
-
 
 plt.show()
